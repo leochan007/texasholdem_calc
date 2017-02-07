f31fe966f97ce95ba82833f36137317ed1c1c1d2
leochan007 <leochan007@github.com>
2017-02-07 14:56:41 +0800

    new demo

--- spark_demo.py
+++ spark_demo.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+from pyspark import SparkContext
 
 if __name__ == '__main__' :
+
+    logFile = "README.md"  # Should be some file on your system
+    sc = SparkContext("local", "Simple App")
+    logData = sc.textFile(logFile).cache()
+
+    numAs = logData.filter(lambda s: 'a' in s).count()
+    numBs = logData.filter(lambda s: 'q' in s).count()
+
+    print("Lines with a: %i, lines with q: %i" % (numAs, numBs))
+
+    sc.stop()
+    
